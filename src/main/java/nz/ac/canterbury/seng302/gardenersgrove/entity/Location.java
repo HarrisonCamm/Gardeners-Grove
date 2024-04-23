60f2aaf53e9d03f83edc12814827924aef5a191f
Oliver Clark <ocl28@uclive.ac.nz>
2024-04-23 14:36:44 +1200

    forgot to add setter back in

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/entity/Location.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/entity/Location.java
@@ -49,6 +49,10 @@ public class Location {
         return streetAddress;
     }
 
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
     public String getSuburb() {
         return suburb;
     }
