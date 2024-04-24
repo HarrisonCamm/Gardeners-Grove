ff964803d91addf9d62275583a3e3b21fb8cb688
Oliver Clark <ocl28@uclive.ac.nz>
2024-04-24 14:27:40 +1200

    figuring out post mapping for the backend

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/AddPlantPictureController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/AddPlantPictureController.java
@@ -1,13 +1,22 @@
 package nz.ac.canterbury.seng302.gardenersgrove.controller;
 
+import nz.ac.canterbury.seng302.gardenersgrove.entity.Garden;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.Plant;
 import nz.ac.canterbury.seng302.gardenersgrove.service.PlantService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Optional;
+
 public class AddPlantPictureController {
+
+    Logger logger = LoggerFactory.getLogger(AddPlantPictureController.class);
+
 
     private final PlantService plantService;
 
@@ -16,10 +25,13 @@ public class AddPlantPictureController {
     }
 
     @PostMapping("/add-plant-picture")
-    public String addPlantPicture(@RequestParam("file") MultipartFile file, @RequestParam("plantId") Long plantId) {
+    public String addPlantPicture(@RequestParam("file") MultipartFile file,
+                                  @RequestParam("plantId") Long plantID) {
+
+        logger.info("POST /add-plant-picture");
 
         // Write the picture to file system
-        Plant plant = plantService.findPlant(plantId).get();
+        Plant plant = plantService.findPlant(plantID).get();
         plant.setPicture(String.valueOf(file));
         return "viewGardenDetailsTemplate";
     }
--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/security/SecurityConfiguration.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/security/SecurityConfiguration.java
@@ -72,7 +72,7 @@ public class SecurityConfiguration {
                     .hasRole("ADMIN")
 
                     // Increase access to authenticated users to reach the "/main", "/view-user-profile", "/edit-user-profile" pages
-                    .requestMatchers("/main", "/view-user-profile", "/edit-user-profile", "/create-garden", "/view-garden", "/view-gardens", "/create-plant", "/edit-plant")
+                    .requestMatchers("/main", "/view-user-profile", "/edit-user-profile", "/create-garden", "/view-garden", "/view-gardens", "/create-plant", "/edit-plant", "/add-plant-picture")
                     .hasRole("USER")
 
                     // Any other request requires authentication
