a750fcb298bfb6ad669151369f45ff770ae33475
Toby Oliver <tol21@uclive.ac.nz>
2024-04-26 14:28:36 +1200

    Did some refactoring and javadoc

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/EditPlantController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/EditPlantController.java
@@ -24,7 +24,6 @@ import static nz.ac.canterbury.seng302.gardenersgrove.validation.PlantValidator.
 
 @Controller
 public class EditPlantController {
-
     Logger logger = LoggerFactory.getLogger(EditPlantController.class);
 
     private final PlantService plantService;
@@ -42,12 +41,10 @@ public class EditPlantController {
         logger.info("GET /edit-plant");
         RedirectService.addEndpoint("/edit-plant?plantID=" + plantID);
 
-        Optional<Plant> found = plantService.findPlant(plantID);
-        if (found.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Plant with ID " + plantID + " not found");
-        }
-        Plant plant = found.get();
+        //Attempt to retrieve plant or throw ResponseStatusException
+        Plant plant = retrievePlant(plantID, plantService);
 
+        //Converts the datePlanted into a string if it is not null from plant object
         String date = "";
         if (plant.getDatePlanted() != null) {
             date = new SimpleDateFormat("yyyy-MM-dd").format(plant.getDatePlanted());
@@ -72,17 +69,17 @@ public class EditPlantController {
                              BindingResult bindingResult,
                              Model model) throws Exception {
         logger.info("PUT /edit-plant");
-        Optional<Plant> found = plantService.findPlant(plantID);
-        if (found.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Plant with ID " + plantID + " not found");
-        }
-        Plant plant = found.get();
+
+        //Attempt to retrieve plant or throw ResponseStatusException
+        Plant plant = retrievePlant(plantID, plantService);
 
         //Validates input fields
         checkName(newPlant.getName(), bindingResult);
         checkDescription(newPlant.getDescription(), bindingResult);
         checkCount(newPlant.getCount(), bindingResult);
 
+        //Parses the datePlanted string into a Date object or leave as null if date is invalid
+        //Currently a dateError can never be caused
         Date date = null;
         if (datePlanted != null && !datePlanted.trim().isEmpty()) {
             try {
@@ -91,13 +88,14 @@ public class EditPlantController {
                 bindingResult.addError(new ObjectError(datePlanted, "Date should be in the format dd/mm/yyyy"));
             }
         }
+
+        //Sets assigns the new values to the original plant object ready to be saved to the database
         plant.setDatePlanted(date);
         plant.setName(newPlant.getName());
         plant.setCount(newPlant.getCount());
         plant.setDescription(newPlant.getDescription());
 
         model.addAttribute("plantID", plantID); // Add gardenID to the model
-
         //Ternary operator to assign null date or assign a formatted date
         model.addAttribute("datePlanted", (date != null) ? new SimpleDateFormat("yyyy-MM-dd").format(date) : "");
 
@@ -109,6 +107,12 @@ public class EditPlantController {
         }
     }
 
+    /**
+     * Validates a string for a plant name
+     * Adds an objectError to the binding result if the name is invalid
+     * @param name A string representing a plant name
+     * @param bindingResult The BindingResult object errors should be bound to
+     */
     private void checkName(String name, BindingResult bindingResult) {
         ObjectError nameError = validatePlantName(name);
         if (nameError != null) {
@@ -116,6 +120,12 @@ public class EditPlantController {
         }
     }
 
+    /**
+     * Validates a string for a plant count
+     * Adds an objectError to the binding result if the count is invalid
+     * @param count A String representing an integer count
+     * @param bindingResult The BindingResult object errors should be bound to
+     */
     private void checkCount(String count, BindingResult bindingResult) {
         ObjectError countError = validatePlantCount(count);
         if (countError != null) {
@@ -123,11 +133,33 @@ public class EditPlantController {
         }
     }
 
+    /**
+     * Validates  a string for a plant description
+     * Adds an object error to the binding result if the description is invalid
+     * @param description   A string representing a plant description
+     * @param bindingResult The BindingResult object errors should be bound to
+     */
     private void checkDescription(String description, BindingResult bindingResult) {
         ObjectError descriptionError = validatePlantDescription(description);
         if (descriptionError != null) {
             bindingResult.addError(descriptionError);
         }
+    }
+
+    /**
+     * Use to retrieve a plant from the database or throw a ResponseStatusException if the plant is not found
+     * @param plantID Long id should be an ID of an existing plant in the database
+     * @param plantService A PlantService object used to interact with the database
+     * @return The retrieved Plant object
+     */
+    private Plant retrievePlant(Long plantID, PlantService plantService) {
+        Optional<Plant> found = plantService.findPlant(plantID);
+        if (found.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Plant with ID " + plantID + " not found");
+        }
+        Plant plant = found.get();
+
+        return plant;
     }
 
 }
