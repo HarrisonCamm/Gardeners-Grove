e875c189a74307012676779800b987f84e93dc10
Zak Lockett <zlo18@uclive.ac.nz>
2024-04-26 17:08:11 +1200

    Add import

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/integration/controller/AutocompleteControllerTests.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/integration/controller/AutocompleteControllerTests.java
@@ -14,6 +14,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
