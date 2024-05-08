48e6b607cbdbebe78af4ab4817eb03d73a23c3f3
Bella Hill <ihi17@uclive.ac.nz>
2024-05-09 11:36:28 +1200

    u11 create plants - fixed the enter form issue, turns out it was with the navbar - fixed in create plant and the user stories that had already passed as we didn't realise this issue had happened in the user forms because of the navbar - also removed all the enter form js from the templates and replaced with reference to js file

--- src/main/resources/static/javascript/prevent-enter-submit.js
+++ src/main/resources/static/javascript/prevent-enter-submit.js
@@ -1,13 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('form').addEventListener('keydown', function(event) {
-        let submitButton = document.getElementById("submitButton");
-        let cancelButton = document.getElementById("cancelButton");
-        if (event.key === 'Enter' && document.activeElement !== submitButton) {
-            if(document.activeElement === cancelButton) {
-                cancelButton.click();
+    // Get all forms within the document
+    const forms = document.querySelectorAll('form');
+
+    // Iterate over each form
+    forms.forEach(function(form) {
+        // Add event listener for keydown event
+        form.addEventListener('keydown', function(event) {
+            let submitButton = form.querySelector('[type="submit"]');
+            let cancelButton = form.querySelector('[type="button"]');
+
+            // Check if the pressed key is Enter and the active element is not the submit button
+            if (event.key === 'Enter' && document.activeElement !== submitButton) {
+                if (document.activeElement === cancelButton) {
+                    cancelButton.click();
+                }
+                event.preventDefault();
+                return false;
             }
-            event.preventDefault();
-            return false;
-        }
+        });
     });
 });
