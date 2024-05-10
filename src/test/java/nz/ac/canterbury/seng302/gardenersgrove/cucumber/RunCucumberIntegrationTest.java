3ea74a5026be93fa149099599fd6ca6a5d3f5d51
Oliver Clark <ocl28@uclive.ac.nz>
2024-05-10 13:40:34 +1200

    pushing for pair programming

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/cucumber/RunCucumberIntegrationTest.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/cucumber/RunCucumberIntegrationTest.java
@@ -1,15 +1,15 @@
 package nz.ac.canterbury.seng302.gardenersgrove.cucumber;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.platform.engine.Constants;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-import io.cucumber.spring.CucumberContextConfiguration;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@RunWith(Cucumber.class)
-@CucumberOptions(features = "nz/ac/canterbury/seng302/gardenersgrove/cucumber")
-@CucumberContextConfiguration
-@SpringBootTest
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("nz/ac/canterbury/seng302/gardenersgrove/cucumber")
+@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME,value = "nz/ac/canterbury/seng302/gardenersgrove/cucumber/step_definitions")
+@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME,value = "pretty, html:target/cucumber-report/cucumber.html")
 public class RunCucumberIntegrationTest {
 }
