c44eb786e4d79e62643d995a636465ef088d44af
Oliver Clark <ocl28@uclive.ac.nz>
2024-05-19 14:56:59 +1200

    Friend request service and repo layer classes for persisting friend requests in the database

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/ManageFriendsController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/ManageFriendsController.java
@@ -2,6 +2,7 @@ package nz.ac.canterbury.seng302.gardenersgrove.controller;
 
 
 import nz.ac.canterbury.seng302.gardenersgrove.entity.User;
+import nz.ac.canterbury.seng302.gardenersgrove.service.FriendRequestService;
 import nz.ac.canterbury.seng302.gardenersgrove.service.RedirectService;
 import nz.ac.canterbury.seng302.gardenersgrove.service.UserService;
 import org.slf4j.Logger;
@@ -21,10 +22,12 @@ public class ManageFriendsController {
     Logger logger = LoggerFactory.getLogger(ManageFriendsController.class);
 
     private UserService userService;
+    private FriendRequestService friendRequestService;
 
     @Autowired
-    public ManageFriendsController(UserService userService) {
+    public ManageFriendsController(UserService userService, FriendRequestService friendRequestService) {
         this.userService = userService;
+        this.friendRequestService = friendRequestService;
     }
 
 
@@ -43,11 +46,36 @@ public class ManageFriendsController {
         return "manageFriendsTemplate";
     }
 
+    /**
+     * Handles page post mappings for searching for users and inviting users to be friends
+     * @param action the action to be performed (search or invite)
+     * @param email the email of the user to be invited
+     * @param searchQuery the search query to search for users
+     * @param model the model to add attributes to
+     * @return the template to be rendered
+     */
     @PostMapping("/manage-friends")
-    public String searchForFriends(@RequestParam(name="searchQuery") String searchQuery,
-                                   Model model) {
+    public String handlePostRequest(@RequestParam(name="action") String action,
+                                    @RequestParam(name="email", required = false) String email,
+                                    @RequestParam(name="searchQuery", required = false) String searchQuery,
+                                    Model model) {
 
-        logger.info("POST /manage-friends");
+        if (action.equals("search")) {
+            return handleSearchRequest(searchQuery, model);
+        } else if (action.equals("invite")) {
+            return handleInviteRequest(email, model);
+        }
+        return "";
+    }
+
+    /**
+     * Handles the search request
+     * @param searchQuery the search query to search for users
+     * @param model the model to add attributes to
+     * @return the template to be rendered
+     */
+    private String handleSearchRequest(String searchQuery, Model model) {
+        logger.info("POST /manage-friends (search)");
 
         List<User> searchedUsers = userService.searchForUsers(searchQuery.toLowerCase());
 
@@ -62,5 +90,23 @@ public class ManageFriendsController {
 
         return "manageFriendsTemplate";
     }
+
+    /**
+     * Handles the invite request
+     * @param email the email of the user to be invited
+     * @param model the model to add attributes to
+     * @return the template to be rendered
+     */
+    private String handleInviteRequest(String email, Model model) {
+        logger.info("POST /manage-friends (invite)");
+
+        User currentUser = userService.getAuthenicatedUser();
+        User invitedUser = userService.getUserByEmail(email);
+
+        friendRequestService.sendRequest(currentUser, invitedUser);
+
+        return "manageFriendsTemplate";
+    }
+
 
 }
