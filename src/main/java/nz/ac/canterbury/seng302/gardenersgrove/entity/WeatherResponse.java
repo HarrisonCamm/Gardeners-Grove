3e47279ceb370256f2196bd09fda8ee2e9fb7f4d
Toby Oliver <tol21@uclive.ac.nz>
2024-07-24 16:29:20 +1200

    Added bean to allow removal of unused json attributes hopefully :nervous:

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/entity/WeatherResponse.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/entity/WeatherResponse.java
@@ -1,11 +1,13 @@
 package nz.ac.canterbury.seng302.gardenersgrove.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class WeatherResponse {
 
     @JsonDeserialize
