b68ee9bd4b330dfe75702c930c4647111a46605e
Toby Oliver <tol21@uclive.ac.nz>
2024-07-26 16:04:53 +1200

    oops late commit :silly: removed more unused json response variables from weather response

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/entity/WeatherResponse.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/entity/WeatherResponse.java
@@ -15,36 +15,16 @@ public class WeatherResponse {
     private WeatherInfo[] weatherInfo;
 
     @JsonDeserialize
-    @JsonProperty("base")
-    private String base;
-
-    @JsonDeserialize
     @JsonProperty("main")
     private MainInfo mainInfo;
-
-    @JsonDeserialize
-    @JsonProperty("visibility")
-    private int visibility;
 
     @JsonDeserialize
     @JsonProperty("dt")
     private Long date;
 
     @JsonDeserialize
-    @JsonProperty("sys")
-    private Sys sys;
-
-    @JsonDeserialize
-    @JsonProperty("timezone")
-    private int timezone;
-
-    @JsonDeserialize
     @JsonProperty("id")
     private int id;
-
-    @JsonDeserialize
-    @JsonProperty("name")
-    private String name;
 
     @JsonDeserialize
     @JsonProperty("cod")
@@ -76,28 +56,8 @@ public class WeatherResponse {
         return dayDateFormat.format(date * 1000);
     }
 
-    public String getBase() {
-        return base;
-    }
-
-    public int getVisibility() {
-        return visibility;
-    }
-
-    public Sys getSys() {
-        return sys;
-    }
-
-    public int getTimezone() {
-        return timezone;
-    }
-
     public int getId() {
         return id;
-    }
-
-    public String getName() {
-        return name;
     }
 
     public int getCod() {
@@ -113,35 +73,15 @@ public class WeatherResponse {
     }
 
     // Inner classes for nested objects
-
-
-
     @JsonDeserialize
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public static class MainInfo {
 
         @JsonProperty("temp")
         public double temperature;
 
-        @JsonProperty("feels_like")
-        public double feelsLike;
-
-        @JsonProperty("temp_min")
-        public double tempMin;
-
-        @JsonProperty("temp_max")
-        public double tempMax;
-
-        @JsonProperty("pressure")
-        public int pressure;
-
         @JsonProperty("humidity")
         public int humidity;
-
-        @JsonProperty("sea_level")
-        public int seaLevel;
-
-        @JsonProperty("grnd_level")
-        public int grndLevel;
 
         public MainInfo() {
             // no-args jackson constructor
@@ -149,10 +89,8 @@ public class WeatherResponse {
     }
 
     @JsonDeserialize
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public static class WeatherInfo {
-
-        @JsonProperty("id")
-        public int id;
 
         @JsonProperty("main")
         public String mainDescription;
@@ -164,29 +102,6 @@ public class WeatherResponse {
         public String icon;
 
         public WeatherInfo() {
-            // no-args jackson constructor
-        }
-    }
-
-    @JsonDeserialize
-    public static class Sys {
-
-        @JsonProperty("type")
-        public int type;
-
-        @JsonProperty("id")
-        public int id;
-
-        @JsonProperty("country")
-        public String country;
-
-        @JsonProperty("sunrise")
-        public Long sunrise;
-
-        @JsonProperty("sunset")
-        public Long sunset;
-
-        public Sys() {
             // no-args jackson constructor
         }
     }
