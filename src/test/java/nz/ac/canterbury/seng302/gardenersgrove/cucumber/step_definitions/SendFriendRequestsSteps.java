f2813f0efd8c623a2d913bdd417bc7a10f4257a8
Bella Hill <ihi17@uclive.ac.nz>
2024-07-26 18:38:31 +1200

    U17 cucumber tests - finished feature file, added blank step definitions.

---
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/cucumber/step_definitions/SendFriendRequestsSteps.java
@@ -0,0 +1,178 @@
+package nz.ac.canterbury.seng302.gardenersgrove.cucumber.step_definitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class SendFriendRequestsSteps {
+    @Given("I am anywhere on the app")
+    public void i_am_anywhere_on_the_app() {
+;
+    }
+
+    @When("I click on a UI element that allows me to send friend requests")
+    public void iClickOnAUIElementThatAllowsMeToSendFriendRequests() {
+        
+    }
+
+    @Then("I am shown a {string} page")
+    public void iAmShownAPage(String arg0) {
+        
+    }
+
+    @Given("I am on the manage friends page")
+    public void iAmOnTheManageFriendsPage() {
+        
+    }
+
+    @When("the page is loaded")
+    public void thePageIsLoaded() {
+    }
+
+    @Then("I see the list of my friends with their names and their profile pictures")
+    public void iSeeTheListOfMyFriendsWithTheirNamesAndTheirProfilePictures() {
+        
+    }
+
+    @And("a link to their gardens list including private and public gardens")
+    public void aLinkToTheirGardensListIncludingPrivateAndPublicGardens() {
+        
+    }
+
+    @When("I hit the add friend button")
+    public void iHitTheAddFriendButton() {
+        
+    }
+
+    @Then("I see a search bar")
+    public void iSeeASearchBar() {
+        
+    }
+
+    @And("I have opened the search bar")
+    public void iHaveOpenedTheSearchBar() {
+        
+    }
+
+    @When("I enter a full name \\(first and last name, if any)")
+    public void iEnterAFullNameFirstAndLastNameIfAny() {
+    }
+
+    @And("I hit the search button")
+    public void iHitTheSearchButton() {
+        
+    }
+
+    @Then("I can see a list of users of the app exactly matching the name I provided")
+    public void iCanSeeAListOfUsersOfTheAppExactlyMatchingTheNameIProvided() {
+        
+    }
+
+    @When("I enter an email address")
+    public void iEnterAnEmailAddress() {
+        
+    }
+
+    @Then("I can see a list of users of the app exactly matching the email provided")
+    public void iCanSeeAListOfUsersOfTheAppExactlyMatchingTheEmailProvided() {
+        
+    }
+
+    @When("I enter a search string")
+    public void iEnterASearchString() {
+        
+    }
+
+    @And("I press the search button")
+    public void iPressTheSearchButton() {
+        
+    }
+
+    @And("there are no perfect matches")
+    public void thereAreNoPerfectMatches() {
+        
+    }
+
+    @Then("I see a message saying {string}")
+    public void iSeeAMessageSaying(String arg0) {
+        
+    }
+
+    @Given("I see a matching person for the search I made")
+    public void iSeeAMatchingPersonForTheSearchIMade() {
+        
+    }
+
+    @When("I hit the {string} button")
+    public void iHitTheButton(String arg0) {
+        
+    }
+
+    @Then("the other user receives an invite that will be shown in their {string} page")
+    public void theOtherUserReceivesAnInviteThatWillBeShownInTheirPage(String arg0) {
+        
+    }
+
+    @And("I have pending invites")
+    public void iHavePendingInvites() {
+        
+    }
+
+    @When("I accept an invite")
+    public void iAcceptAnInvite() {
+        
+    }
+
+    @Then("that person is added to my list of friends")
+    public void thatPersonIsAddedToMyListOfFriends() {
+        
+    }
+
+    @And("I can see their profile")
+    public void iCanSeeTheirProfile() {
+        
+    }
+
+    @And("I am added to that person’s friends list")
+    public void iAmAddedToThatPersonSFriendsList() {
+        
+    }
+
+    @And("that person can see my profile")
+    public void thatPersonCanSeeMyProfile() {
+        
+    }
+
+    @When("I decline an invite")
+    public void iDeclineAnInvite() {
+        
+    }
+
+    @Then("that person is not added to my list of friends")
+    public void thatPersonIsNotAddedToMyListOfFriends() {
+        
+    }
+
+    @And("they cannot invite me anymore")
+    public void theyCannotInviteMeAnymore() {
+        
+    }
+
+    @Given("I have sent an invite")
+    public void iHaveSentAnInvite() {
+        
+    }
+
+    @When("I check the status of the invite")
+    public void iCheckTheStatusOfTheInvite() {
+        
+    }
+
+    @Then("I can see the status of the invite as one of {string}, or {string}")
+    public void iCanSeeTheStatusOfTheInviteAsOneOfOr(String arg0, String arg1) {
+    }
+}
--- src/test/resources/nz/ac/canterbury/seng302/gardenersgrove/cucumber/u17-send-friend-requests.feature
+++ src/test/resources/nz/ac/canterbury/seng302/gardenersgrove/cucumber/u17-send-friend-requests.feature
@@ -3,12 +3,12 @@ Feature: U17 As Liam, I want to connect with my friends on Gardener’s Grove so
   Scenario: AC1 - Viewing the Manage Friends page.
     Given I am anywhere on the app
     When I click on a UI element that allows me to send friend requests
-    Then I am shown a “manage friends” page
+    Then I am shown a "manage friends" page
 
   Scenario: AC2 - Viewing list of friends
-    Given I am on the manage friends page,
-    Then I see the list of my friends with their names
-    And their profile pictures
+    Given I am on the manage friends page
+    When the page is loaded
+    Then I see the list of my friends with their names and their profile pictures
     And a link to their gardens list including private and public gardens
 
   Scenario: AC3 - Viewing Search Bar
@@ -16,16 +16,50 @@ Feature: U17 As Liam, I want to connect with my friends on Gardener’s Grove so
     When I hit the add friend button
     Then I see a search bar
 
-  Scenario: AC4 -
+  Scenario: AC4 - Viewing list of users from name search
     Given I am on the manage friends page
     And I have opened the search bar
     When I enter a full name (first and last name, if any)
     And I hit the search button
     Then I can see a list of users of the app exactly matching the name I provided
 
-  Scenario: AC5
+  Scenario: AC5 - Viewing list of users from email search
     Given I am on the manage friends page
     And I have opened the search bar
     When I enter an email address
     And I hit the search button
     Then I can see a list of users of the app exactly matching the email provided
+
+  Scenario: AC6 - No perfect matches from search
+    Given I am on the manage friends page
+    And I have opened the search bar
+    When I enter a search string
+    And I press the search button
+    And there are no perfect matches
+    Then I see a message saying "There is nobody with that name or email in Gardener’s Grove"
+
+  Scenario: AC7 - Adding a friend from search
+    Given I see a matching person for the search I made
+    When I hit the "invite as friend" button
+    Then the other user receives an invite that will be shown in their "manage friends" page
+
+  Scenario: AC8 - Accepting a friend request
+    Given I am on the manage friends page
+    And I have pending invites
+    When I accept an invite
+    Then that person is added to my list of friends
+    And I can see their profile
+    And I am added to that person’s friends list
+    And that person can see my profile
+
+  Scenario: AC9 - Declining a friend request
+    Given I am on the manage friends page
+    And I have pending invites
+    When I decline an invite
+    Then that person is not added to my list of friends
+    And they cannot invite me anymore
+
+  Scenario: AC10 - Viewing friend request status
+    Given I have sent an invite
+    When I check the status of the invite
+    Then I can see the status of the invite as one of "pending", or "declined"
