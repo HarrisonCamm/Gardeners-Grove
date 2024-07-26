121d6e92723d8f1971ce20c4cf6c910f5d0842cf
Bella Hill <ihi17@uclive.ac.nz>
2024-07-26 18:56:05 +1200

    U17 cucumber tests - changed format of step definition names to be consistent with rest of code

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/cucumber/step_definitions/SendFriendRequestsSteps.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/cucumber/step_definitions/SendFriendRequestsSteps.java
@@ -15,164 +15,164 @@ public class SendFriendRequestsSteps {
     }
 
     @When("I click on a UI element that allows me to send friend requests")
-    public void iClickOnAUIElementThatAllowsMeToSendFriendRequests() {
+    public void i_click_on_a_ui_element_that_allows_me_to_send_friend_requests() {
         
     }
 
     @Then("I am shown a {string} page")
-    public void iAmShownAPage(String arg0) {
+    public void i_am_shown_a_page(String arg0) {
         
     }
 
     @Given("I am on the manage friends page")
-    public void iAmOnTheManageFriendsPage() {
+    public void i_am_on_the_manage_friends_page() {
         
     }
 
     @When("the page is loaded")
-    public void thePageIsLoaded() {
+    public void the_page_is_loaded() {
     }
 
     @Then("I see the list of my friends with their names and their profile pictures")
-    public void iSeeTheListOfMyFriendsWithTheirNamesAndTheirProfilePictures() {
+    public void i_see_the_list_of_my_friends_with_their_names_and_their_profile_pictures() {
         
     }
 
     @And("a link to their gardens list including private and public gardens")
-    public void aLinkToTheirGardensListIncludingPrivateAndPublicGardens() {
+    public void a_link_to_their_gardens_list_including_private_and_public_gardens() {
         
     }
 
     @When("I hit the add friend button")
-    public void iHitTheAddFriendButton() {
+    public void i_hit_the_add_friend_button() {
         
     }
 
     @Then("I see a search bar")
-    public void iSeeASearchBar() {
+    public void i_see_a_search_bar() {
         
     }
 
     @And("I have opened the search bar")
-    public void iHaveOpenedTheSearchBar() {
+    public void i_have_opened_the_search_bar() {
         
     }
 
     @When("I enter a full name \\(first and last name, if any)")
-    public void iEnterAFullNameFirstAndLastNameIfAny() {
+    public void i_enter_a_full_name_first_and_last_name_if_any() {
     }
 
     @And("I hit the search button")
-    public void iHitTheSearchButton() {
+    public void i_hit_the_search_button() {
         
     }
 
     @Then("I can see a list of users of the app exactly matching the name I provided")
-    public void iCanSeeAListOfUsersOfTheAppExactlyMatchingTheNameIProvided() {
+    public void i_can_see_a_list_of_users_of_the_app_exactly_matching_the_name_i_provided() {
         
     }
 
     @When("I enter an email address")
-    public void iEnterAnEmailAddress() {
+    public void i_enter_an_email_address() {
         
     }
 
     @Then("I can see a list of users of the app exactly matching the email provided")
-    public void iCanSeeAListOfUsersOfTheAppExactlyMatchingTheEmailProvided() {
+    public void i_can_see_a_list_of_users_of_the_app_exactly_matching_the_email_provided() {
         
     }
 
     @When("I enter a search string")
-    public void iEnterASearchString() {
+    public void i_enter_a_search_string() {
         
     }
 
     @And("I press the search button")
-    public void iPressTheSearchButton() {
+    public void i_press_the_search_button() {
         
     }
 
     @And("there are no perfect matches")
-    public void thereAreNoPerfectMatches() {
+    public void there_are_no_perfect_matches() {
         
     }
 
     @Then("I see a message saying {string}")
-    public void iSeeAMessageSaying(String arg0) {
+    public void i_see_a_message_saying(String arg0) {
         
     }
 
     @Given("I see a matching person for the search I made")
-    public void iSeeAMatchingPersonForTheSearchIMade() {
+    public void i_see_a_matching_person_for_the_search_i_made() {
         
     }
 
     @When("I hit the {string} button")
-    public void iHitTheButton(String arg0) {
+    public void i_hit_the_button(String arg0) {
         
     }
 
     @Then("the other user receives an invite that will be shown in their {string} page")
-    public void theOtherUserReceivesAnInviteThatWillBeShownInTheirPage(String arg0) {
+    public void the_other_user_receives_an_invite_that_will_be_shown_in_their_page(String arg0) {
         
     }
 
     @And("I have pending invites")
-    public void iHavePendingInvites() {
+    public void i_have_pending_invites() {
         
     }
 
     @When("I accept an invite")
-    public void iAcceptAnInvite() {
+    public void i_accept_an_invite() {
         
     }
 
     @Then("that person is added to my list of friends")
-    public void thatPersonIsAddedToMyListOfFriends() {
+    public void that_person_is_added_to_my_list_of_friends() {
         
     }
 
     @And("I can see their profile")
-    public void iCanSeeTheirProfile() {
+    public void i_can_see_their_profile() {
         
     }
 
     @And("I am added to that person’s friends list")
-    public void iAmAddedToThatPersonSFriendsList() {
+    public void i_am_added_to_that_persons_friends_list() {
         
     }
 
     @And("that person can see my profile")
-    public void thatPersonCanSeeMyProfile() {
+    public void that_person_can_see_my_profile() {
         
     }
 
     @When("I decline an invite")
-    public void iDeclineAnInvite() {
+    public void i_decline_an_invite() {
         
     }
 
     @Then("that person is not added to my list of friends")
-    public void thatPersonIsNotAddedToMyListOfFriends() {
+    public void that_person_is_not_added_to_my_list_of_friends() {
         
     }
 
     @And("they cannot invite me anymore")
-    public void theyCannotInviteMeAnymore() {
+    public void they_cannot_invite_me_anymore() {
         
     }
 
     @Given("I have sent an invite")
-    public void iHaveSentAnInvite() {
+    public void i_have_sent_an_invite() {
         
     }
 
     @When("I check the status of the invite")
-    public void iCheckTheStatusOfTheInvite() {
+    public void i_check_the_status_of_the_invite() {
         
     }
 
     @Then("I can see the status of the invite as one of {string}, or {string}")
-    public void iCanSeeTheStatusOfTheInviteAsOneOfOr(String arg0, String arg1) {
+    public void i_can_see_the_status_of_the_invite_as_one_of_or(String arg0, String arg1) {
     }
 }
