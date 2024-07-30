a0fa88b7832a7a5f24cea7da2ec84455569fb4d8
Aakrista Dahal <ada185@uclive.ac.nz>
2024-07-30 14:32:47 +1200

    U11-Create-Plant form.
    Trying to make the javascript work for all input fields so that we don't have to worry about having a character limit for each type of input field.

--- src/main/resources/static/javascript/description-length.js
+++ src/main/resources/static/javascript/description-length.js
@@ -8,3 +8,13 @@ function descriptionLength(input) {
         descriptionLengthWarning.textContent = "";
     }
 }
+function countLength(input) {
+    let description = input.value;
+    let descriptionLength = description.length;
+    let descriptionLengthWarning = document.getElementById('descriptionLengthValidation');
+    if (descriptionLength > 512) {
+        descriptionLengthWarning.textContent = "Plant description must be less than 512 characters";
+    } else {
+        descriptionLengthWarning.textContent = "";
+    }
+}
