0a7295a27eff0441c06eccf3479397f460c51257
hrc48 <hrc48@uclive.ac.nz>
2024-07-31 15:15:15 +1200

    Individual buttons now close when clicked instead of them all being toggled by one variable. Still unideal implementation

--- src/main/resources/static/javascript/popup-notification.js
+++ src/main/resources/static/javascript/popup-notification.js
@@ -1,7 +1,17 @@
-function toggleAlert() {
-    const form = document.querySelector(".alert");
+function toggleWeatherAlert() {
+    const form = document.querySelector(".weather-alert");
     form.hidden = !form.hidden;
 }
+
+function toggleWaterAlert() {
+    const form = document.querySelector(".water-alert");
+    form.hidden = !form.hidden;
+}
+
+//TODO: Find a way to pass an id to an alertFragment so we can reuse it
+// and not have to use two copies of essentially the same fragment
+
+
 
 // function toggleAlert(id) {
 //     console.log(`ID is ${id}`);
