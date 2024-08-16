0719026e7e5bf855fcd17914d84c8506792ce074
Oliver Clark <ocl28@uclive.ac.nz>
2024-08-16 14:52:51 +1200

    Got it working with hardcoding values. If you send a message from startup@user.com then sarah@email.com will receive it :))))))))

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/MessagesController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/MessagesController.java
@@ -1,11 +1,17 @@
 package nz.ac.canterbury.seng302.gardenersgrove.controller;
 
 import nz.ac.canterbury.seng302.gardenersgrove.entity.Message;
+import nz.ac.canterbury.seng302.gardenersgrove.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Date;
@@ -15,18 +21,27 @@ public class MessagesController {
 
     Logger logger = LoggerFactory.getLogger(MessagesController.class);
 
+    private final UserService userService;
+    private final SimpMessagingTemplate messagingTemplate;
+
+    @Autowired
+    public MessagesController(UserService userService, SimpMessagingTemplate messagingTemplate) {
+        this.userService = userService;
+        this.messagingTemplate = messagingTemplate;
+    }
+
     @GetMapping("/messages")
-    public String getMessages() {
+    public String getMessages(Model model) {
         logger.info("/GET messages");
+
+        model.addAttribute("from", userService.getAuthenticatedUser().getEmail());
+
         return "messagesTemplate";
     }
 
-    @MessageMapping("/chat.send")
-    @SendTo("/topic/public")
-    public Message sendMessage(Message message) {
+    @MessageMapping("/chat.send/{username}")
+    public void sendMessage(@DestinationVariable String username, Message message) {
         message.setTimestamp(new Date());
-        return message;
+        messagingTemplate.convertAndSendToUser(username, "/queue/reply", message);
     }
-
-
 }
