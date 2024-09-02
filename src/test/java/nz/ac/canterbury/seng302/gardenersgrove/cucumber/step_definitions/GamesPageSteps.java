2eb47b94dfb5f744d02344589e47b91a572daa96
Aakrista Dahal <ada185@uclive.ac.nz>
2024-09-02 16:41:11 +1200

    U6004 - Games page.
    Resolving merge review comments. Change naming conventions and refactored the cucumber tests. Made it so that the users not able to click the play button for plant guesser.

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/cucumber/step_definitions/GamesPageSteps.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/cucumber/step_definitions/GamesPageSteps.java
@@ -6,7 +6,7 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.jupiter.api.Assertions;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -24,33 +24,38 @@ public class GamesPageSteps {
     private WebApplicationContext webApplicationContext;
 
     private static MockMvc mockMvc;
-    private MvcResult mvcResult;
 
     @Before
     public void setup() {
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     }
 
-    @WithMockUser
     @Given("I am logged in")
-    public void i_am_logged_in() throws Exception {
-        mvcResult = mockMvc.perform(get("/main"))
+    public void iAmLoggedIn() throws Exception {
+        MvcResult loginResult = mockMvc.perform(get("/main")
+                        .with(SecurityMockMvcRequestPostProcessors.user("user").roles("USER")))
                 .andExpect(status().isOk())
                 .andExpect(view().name("mainTemplate"))
                 .andReturn();
     }
 
     @When("I click the 'Games' button on the navbar")
-    public void i_click_the_games_button_on_the_navbar() throws Exception {
-        mvcResult = mockMvc.perform(get("/games"))
+    public void iClickTheGamesButtonOnTheNavbar() throws Exception {
+        MvcResult gamesResult = mockMvc.perform(get("/games")
+                        .with(SecurityMockMvcRequestPostProcessors.user("user").roles("USER")))
                 .andExpect(status().isOk())
                 .andExpect(view().name("gamesTemplate"))
                 .andReturn();
     }
 
     @Then("I should see the game {string} with a description")
-    public void i_should_see_the_game_with_a_description(String gameTitle) throws Exception {
-        String content = mvcResult.getResponse().getContentAsString();
+    public void iShouldSeeTheGameWithADescription(String gameTitle) throws Exception {
+        MvcResult gamesResult = mockMvc.perform(get("/games")
+                        .with(SecurityMockMvcRequestPostProcessors.user("user").roles("USER")))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String content = gamesResult.getResponse().getContentAsString();
         boolean hasGameTitle = content.contains("<h5 class=\"card-title\">" + gameTitle + "</h5>");
         boolean hasGameDescription = content.contains("Learn some plant names and images!");
 
@@ -61,24 +66,31 @@ public class GamesPageSteps {
     }
 
     @Then("I should see a {string} button")
-    public void i_should_see_a_button(String buttonText) throws Exception {
-        String content = mvcResult.getResponse().getContentAsString();
-        boolean hasPlayButton = content.contains("<a href=\"/games/play\" class=\"btn btn-primary\">" + buttonText + "</a>");
+    public void iShouldSeeAButton(String buttonText) throws Exception {
+        MvcResult gamesResult = mockMvc.perform(get("/games")
+                        .with(SecurityMockMvcRequestPostProcessors.user("user").roles("USER")))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String content = gamesResult.getResponse().getContentAsString();
+        boolean hasPlayButton = content.contains("<a href=\"/daily-spin\" class=\"btn btn-primary\">" + buttonText + "</a>");
 
         Assertions.assertTrue(hasPlayButton, "Expected to find a '" + buttonText + "' button on the page.");
     }
 
-
-
     @Then("I am taken to the Games page")
-    public void iAmTakenToTheGamesPage() {
-        ModelAndView modelAndView = mvcResult.getModelAndView();
+    public void iAmTakenToTheGamesPage() throws Exception {
+        MvcResult gamesResult = mockMvc.perform(get("/games")
+                        .with(SecurityMockMvcRequestPostProcessors.user("user").roles("USER")))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        ModelAndView modelAndView = gamesResult.getModelAndView();
         String viewName = modelAndView.getViewName();
 
         Assertions.assertEquals("gamesTemplate", viewName, "Expected to be on the Games Page, but was not.");
 
-        int status = mvcResult.getResponse().getStatus();
+        int status = gamesResult.getResponse().getStatus();
         Assertions.assertEquals(200, status, "Expected status 200 (OK), but got: " + status);
     }
-
 }
