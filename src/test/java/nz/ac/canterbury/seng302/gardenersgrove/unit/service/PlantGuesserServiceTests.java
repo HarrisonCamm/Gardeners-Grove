79e765e8c04da4c2b3a962775b1e15b69ce08130
Angelica Silva <ams361@uclive.ac.nz>
2024-09-05 17:27:14 +1200

    U6005 plant guesser - started unit testing for plant guesser service class

---
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/unit/service/PlantGuesserServiceTests.java
@@ -0,0 +1,58 @@
+package nz.ac.canterbury.seng302.gardenersgrove.unit.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import nz.ac.canterbury.seng302.gardenersgrove.entity.PlantData;
+import nz.ac.canterbury.seng302.gardenersgrove.entity.PlantGuesserList;
+import nz.ac.canterbury.seng302.gardenersgrove.service.PlantGuesserService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.web.client.RestTemplate;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+import static org.mockito.ArgumentMatchers.any;
+
+
+public class PlantGuesserServiceTests {
+    private PlantGuesserService plantGuesserService;
+    private static final RestTemplate restTemplate = Mockito.mock(RestTemplate.class);
+
+    private static String plantFamilyResponseJsonString;
+    private static String plantsResponseJsonString;
+    private static PlantGuesserList plantGuesserList;
+
+
+
+    @BeforeAll
+    static public void  jsonSetup() throws Exception {
+        //read json example file
+        plantFamilyResponseJsonString = Files.readString(Paths.get("src/test/resources/json/getPlantFamilyResponse.json"));
+        plantsResponseJsonString = Files.readString(Paths.get("src/test/resources/json/getPlantsResponse.json"));
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        plantGuesserList = objectMapper.readValue(plantsResponseJsonString, PlantGuesserList.class);
+
+    }
+
+    @BeforeEach
+    public void setUp() {
+        //mock any classes here if needed
+        Mockito.when(restTemplate.getForObject(any(String.class), any())).thenReturn(plantGuesserList);
+        plantGuesserService = new PlantGuesserService(restTemplate);
+    }
+
+    @Test
+    public void validApiRequest_ReturnsPlantGuesserList() {
+        int pageNum = 1;
+        PlantGuesserList plantGuesserList = plantGuesserService.getPlantPage(pageNum);
+        Assertions.assertNotNull(plantGuesserList);
+        PlantData[] plantGuesserItems = plantGuesserList.getPlantGuesserList();
+        String plantItemCommonName = Arrays.stream(plantGuesserItems).toList().get(0).common_name;
+        Assertions.assertEquals("Benguet pine", plantItemCommonName);
+    }
+}
