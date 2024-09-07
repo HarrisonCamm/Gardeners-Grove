4b579d50d78a8bf6d41bde1c6ef2bec963ced4e7
Zak Lockett <zlo18@uclive.ac.nz>
2024-09-07 19:06:10 +1200

    Make brand-new contacts appear in the recipient's contact list when they first receive a new message from them

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/MessagesController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/MessagesController.java
@@ -1,5 +1,6 @@
 package nz.ac.canterbury.seng302.gardenersgrove.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.Garden;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.Message;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.User;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Controller
 public class MessagesController {
@@ -32,6 +34,8 @@ public class MessagesController {
     private final GardenService gardenService;
     private final SimpMessagingTemplate messagingTemplate;
     private final MessageService messageService;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     /**
      * Constructor for the MessagesController
@@ -78,12 +82,21 @@ public class MessagesController {
         String currentUserEmail = userService.getAuthenticatedUser().getEmail();
         List<User> friends = userService.getAuthenticatedUser().getFriends();
         List<User> contacts = userService.getAuthenticatedUser().getAllContacts();
-        if (garden != null) {
+        if (garden != null && !contacts.contains(garden.getOwner())) {
             contacts.add(garden.getOwner());
         }
+        friends.forEach(friend -> { friend.setPassword(""); });
+        contacts.forEach(contact -> { contact.setPassword(""); });
         model.addAttribute("from", currentUserEmail);
         model.addAttribute("friends", friends);
         model.addAttribute("contacts", contacts);
+        try {
+            List<String> contactEmails = contacts.stream().map(User::getEmail).toList();
+            model.addAttribute("contactEmails", objectMapper.writeValueAsString(contactEmails));
+        } catch (Exception e) {
+            logger.error("unable to convert contacts list to JSON");
+        }
+
 
         String defaultMessage = "Start a conversation!";
 
@@ -131,6 +144,11 @@ public class MessagesController {
     public @ResponseBody List<Message> getChat(@PathVariable String username) {
         String currentUser = userService.getAuthenticatedUser().getEmail();
         return messageService.getConversation(currentUser, username); // Return the list of messages as JSON
+    }
+
+    @GetMapping("/contacts")
+    public @ResponseBody List<String> getContacts() {
+        return userService.getAuthenticatedUser().getAllContacts().stream().map(User::getEmail).toList();
     }
 
 }
