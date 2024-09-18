3d997edcf397cd05d90335ede5a5ad89f53eb26a
Zak Lockett <zlo18@uclive.ac.nz>
2024-09-18 19:02:25 +1200

    Created Item entity types

---
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/entity/BadgeItem.java
@@ -0,0 +1,24 @@
+package nz.ac.canterbury.seng302.gardenersgrove.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("badge")
+public class BadgeItem extends Item {
+    @Column(length = 32)
+    private String badge;
+
+    protected BadgeItem() {
+    }
+
+    public BadgeItem(String name, boolean equipable, int price, String badge) {
+        super(name, equipable, price);
+        this.badge = badge;
+    }
+
+    public String getBadge() {
+        return badge;
+    }
+}
