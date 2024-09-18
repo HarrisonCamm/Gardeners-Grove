ce3fc70ad318019987e3da587756aa6d6f2b90b5
Oliver Clark <ocl28@uclive.ac.nz>
2024-09-19 11:15:56 +1200

    U6007/inventory-access AC1
    Pair programming w/ Bella. We implemented backend logic to add dummy items to the model, we then render them using Bootstrap grids.
    
    Updated a step def method to do test this.
    
    Had to comment out the adding of items since we need to actually have Item entities in another task

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/InventoryController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/InventoryController.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class InventoryController {
     Logger logger = LoggerFactory.getLogger(InventoryController.class);
@@ -15,6 +18,29 @@ public class InventoryController {
     public String getTemplate(Model model) {
         logger.info("GET /inventory");
         RedirectService.addEndpoint("/inventory");
+
+        //Create and populate a list of items for the view to render
+        List<String[]> badgeItems = new ArrayList<>();
+
+        // TODO - Simulating the adding of items, this will be done using service and repo layers in another task
+        // uncomment these to view how it would look, commented out for tests to work
+//        badgeItems.add(new String[]{"1x", "vegemite.png", "Vegemite"});
+//        badgeItems.add(new String[]{"1x", "timtam.png", "Tim Tam"});
+//        badgeItems.add(new String[]{"1x", "neo_fabian.png", "Neo Fabian"});
+
+        //Create and populate a list of items for the view to render
+        List<String[]> gifItems = new ArrayList<>();
+
+        // TODO - Simulating the adding of items, this will be done using service and repo layers in another task
+//        gifItems.add(new String[]{"1x", "fabian.gif", "Fabian Intensifies"});
+//        gifItems.add(new String[]{"1x", "scrum_master_harrison.gif", "Scrum Master Harrison"});
+//        gifItems.add(new String[]{"1x", "stick_man.gif", "Stick Man"});
+
+
+        model.addAttribute("badgeItems", badgeItems);
+        model.addAttribute("gifItems", gifItems);
+
+
         return "inventoryTemplate";
     }
 }
