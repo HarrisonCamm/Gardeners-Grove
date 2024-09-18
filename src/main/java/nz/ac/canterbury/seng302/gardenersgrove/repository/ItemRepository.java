12def0ba64069e75b21819684a3cb2335938e716
Zak Lockett <zlo18@uclive.ac.nz>
2024-09-18 19:02:50 +1200

    Created Item repository and service for basic Item operations

---
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/repository/ItemRepository.java
@@ -0,0 +1,42 @@
+package nz.ac.canterbury.seng302.gardenersgrove.repository;
+
+import nz.ac.canterbury.seng302.gardenersgrove.entity.Item;
+import nz.ac.canterbury.seng302.gardenersgrove.entity.User;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ItemRepository extends CrudRepository<Item, Long> {
+    @NotNull
+    Optional<Item> findById(@NotNull Long id);
+
+    @NotNull
+    List<Item> findAll();
+
+    Optional<Item> findByName(String name);
+
+    List<Item> findByOwner(User owner);
+
+    @Query("SELECT i FROM Item i WHERE i.itemType = 'badge'")
+    List<Item> findBadges();
+
+    List<Item> findBadgesByOwner(User owner);
+
+    @Query("SELECT i FROM Item i WHERE i.itemType = 'image'")
+    List<Item> findImages();
+
+    List<Item> findImagesByOwner(User owner);
+
+    @Query("SELECT i FROM Item i WHERE i.isEquipable = :isEquipable")
+    List<Item> findIsEquipable(Boolean isEquipable);
+
+    List<Item> findIsEquipableByOwner(User owner);
+
+    List<Item> findIsEquippedByOwner(User owner);
+
+}
---
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/service/ItemService.java
@@ -0,0 +1,74 @@
+package nz.ac.canterbury.seng302.gardenersgrove.service;
+
+import nz.ac.canterbury.seng302.gardenersgrove.entity.Item;
+import nz.ac.canterbury.seng302.gardenersgrove.entity.User;
+import nz.ac.canterbury.seng302.gardenersgrove.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ItemService {
+    private ItemRepository itemRepository;
+
+    @Autowired
+    public ItemService(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public void deleteItem(Item item) {
+        itemRepository.delete(item);
+    }
+
+    public Item getItemById(Long id) {
+        return itemRepository.findById(id).orElse(null);
+    }
+
+    public Item getItemByName(String name) {
+        return itemRepository.findByName(name).orElse(null);
+    }
+
+    public boolean itemExists(String name) {
+        return itemRepository.findByName(name).isPresent();
+    }
+
+    public Iterable<Item> getAllItems() {
+        return itemRepository.findAll();
+    }
+
+    public Iterable<Item> getBadges() {
+        return itemRepository.findBadges();
+    }
+
+    public Iterable<Item> getImages() {
+        return itemRepository.findImages();
+    }
+
+    public Iterable<Item> getEquipable() {
+        return itemRepository.findIsEquipable(true);
+    }
+
+    public Iterable<Item> getEquippedByOwner(User owner) {
+        return itemRepository.findIsEquippedByOwner(owner);
+    }
+
+    public Iterable<Item> getEquipableByOwner(User owner) {
+        return itemRepository.findIsEquipableByOwner(owner);
+    }
+
+    public Iterable<Item> getBadgesByOwner(User owner) {
+        return itemRepository.findBadgesByOwner(owner);
+    }
+
+    public Iterable<Item> getImagesByOwner(User owner) {
+        return itemRepository.findImagesByOwner(owner);
+    }
+
+    public Iterable<Item> getItemsByOwner(User owner) {
+        return itemRepository.findByOwner(owner);
+    }
+
+}
