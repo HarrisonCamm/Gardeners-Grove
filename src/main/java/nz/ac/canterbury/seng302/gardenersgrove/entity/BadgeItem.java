8c6d4a0b2afc4bfbd4ba75dd45c46a9ad37a9845
Zak Lockett <zlo18@uclive.ac.nz>
2024-09-24 19:01:43 +1200

    Re-add attributes and properties removed on another branch

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/entity/BadgeItem.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/entity/BadgeItem.java
@@ -1,25 +1,23 @@
 package nz.ac.canterbury.seng302.gardenersgrove.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 
 // Badge class implementing Purchasable and Equipable
 @Entity
-public class BadgeItem extends Item implements Equipable {
-
-    @Column(nullable = false)
-    private boolean isEquipped;
+@DiscriminatorValue("badge")
+public class BadgeItem extends Item {
 
     @Column(nullable = false)
     private String emoji;
 
-    public BadgeItem() {
+    protected BadgeItem() {
     }
 
     public BadgeItem(String name, Integer price, String emoji, Integer quantity) {
         super(name, price, quantity);
         this.emoji = emoji;
-        this.isEquipped = false;
     }
 
     public String getEmoji() {
@@ -30,13 +28,4 @@ public class BadgeItem extends Item implements Equipable {
         this.emoji = emoji;
     }
 
-    @Override
-    public boolean isEquipped() {
-        return isEquipped;
-    }
-
-    @Override
-    public void setEquipped(boolean equipped) {
-        this.isEquipped = equipped;
-    }
 }
