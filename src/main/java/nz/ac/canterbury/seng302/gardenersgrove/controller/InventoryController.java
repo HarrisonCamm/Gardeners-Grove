d31c2d9aa729373e9fa03fc9d0e8eff0dce33e64
Zak Lockett <zlo18@uclive.ac.nz>
2024-09-24 19:02:16 +1200

    Show actual inventory items on the page for it

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/InventoryController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/InventoryController.java
@@ -1,8 +1,13 @@
 package nz.ac.canterbury.seng302.gardenersgrove.controller;
 
+import nz.ac.canterbury.seng302.gardenersgrove.entity.Item;
+import nz.ac.canterbury.seng302.gardenersgrove.entity.User;
+import nz.ac.canterbury.seng302.gardenersgrove.service.ItemService;
 import nz.ac.canterbury.seng302.gardenersgrove.service.RedirectService;
+import nz.ac.canterbury.seng302.gardenersgrove.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,13 +19,26 @@ import java.util.List;
 public class InventoryController {
     Logger logger = LoggerFactory.getLogger(InventoryController.class);
 
+    private final ItemService itemService;
+    private final UserService userService;
+
+    @Autowired
+    public InventoryController(ItemService itemService, UserService userService) {
+        this.itemService = itemService;
+        this.userService = userService;
+    }
+
     @GetMapping("/inventory")
     public String getTemplate(Model model) {
         logger.info("GET /inventory");
         RedirectService.addEndpoint("/inventory");
 
+        //Get the current user
+        User currentUser = userService.getAuthenticatedUser();
+
         //Create and populate a list of items for the view to render
-        List<String[]> badgeItems = new ArrayList<>();
+//        List<String[]> badgeItems = new ArrayList<>();
+        List<Item> badgeItems = itemService.getBadgesByOwner(currentUser.getUserId());
 
         // TODO - Simulating the adding of items, this will be done using service and repo layers in another task
         // uncomment these to view how it would look, commented out for tests to work
@@ -29,17 +47,28 @@ public class InventoryController {
 //        badgeItems.add(new String[]{"1x", "neo_fabian.png", "Neo Fabian"});
 
         //Create and populate a list of items for the view to render
-        List<String[]> gifItems = new ArrayList<>();
+//        List<String[]> gifItems = new ArrayList<>();
+        List<Item> imageItems = itemService.getImagesByOwner(currentUser.getUserId());
 
         // TODO - Simulating the adding of items, this will be done using service and repo layers in another task
 //        gifItems.add(new String[]{"1x", "fabian.gif", "Fabian Intensifies"});
 //        gifItems.add(new String[]{"1x", "scrum_master_harrison.gif", "Scrum Master Harrison"});
 //        gifItems.add(new String[]{"1x", "stick_man.gif", "Stick Man"});
 
+        if (badgeItems.isEmpty()) {
+            currentUser.addItem(itemService.getItemByName("Happy"));
+            currentUser.addItem(itemService.getItemByName("Eggplant"));
+            userService.addUser(currentUser);
+        }
+        if (imageItems.isEmpty()) {
+            currentUser.addItem(itemService.getItemByName("Cat Fall"));
+            currentUser.addItem(itemService.getItemByName("Cat Typing"));
+            currentUser.addItem(itemService.getItemByName("Fabian Intensifies"));
+            userService.addUser(currentUser);
+        }
 
         model.addAttribute("badgeItems", badgeItems);
-        model.addAttribute("gifItems", gifItems);
-
+        model.addAttribute("imageItems", imageItems);
 
         return "inventoryTemplate";
     }
