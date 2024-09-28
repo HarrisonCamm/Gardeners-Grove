c1c76c922b17ee3c3b49234ddf79dd0fc2fdbfaf
Zak Lockett <zlo18@uclive.ac.nz>
2024-09-28 15:36:07 +1200

    Modified last 2 tests to work with badges being Images

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/unit/entity/UserEntityTest.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/unit/entity/UserEntityTest.java
@@ -5,10 +5,14 @@ import nz.ac.canterbury.seng302.gardenersgrove.entity.Image;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.User;
 import org.h2.util.Utils;
 import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ResourceLoader;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -63,28 +67,40 @@ class UserEntityTest {
         assertNotNull(user.getItem(badgeItem, 1));
         assertEquals(1, user.getItem(badgeItem, 1).getQuantity());
     }
-//TODO make these pass
-//    @Test
-//    void removeItem_RemovesItemFromInventoryWithQuantityZero() {
-//        User user = new User("John", "Doe", false, "", "", "");
-//        BadgeItem badgeItem = new BadgeItem("Badge", 10, null, 3);
-//        user.addItem(badgeItem);
-//        user.removeItem(badgeItem, 0);
-//        assertEquals(1, user.getInventory().size());
-//        assertNotNull(user.getItem(badgeItem, 1));
-//        assertEquals(1, user.getItem(badgeItem, 1).getQuantity());
-//    }
-//
-//    @Test
-//    void removeItem_RemovesItemFromInventoryWithQuantityGreaterThanInventory() {
-//        User user = new User("John", "Doe", false, "", "", "");
-//        BadgeItem badgeItem = new BadgeItem("Badge", 10, null, 3);
-//        user.addItem(badgeItem);
-//        user.addItem(badgeItem);
-//        user.addItem(badgeItem);
-//        user.addItem(badgeItem);
-//        assertThrows(IllegalArgumentException.class, () -> user.removeItem(badgeItem, 5));
-//        assertNotNull(user.getItem(badgeItem, 4));
-//        assertEquals(4, user.getItem(badgeItem, 4).getQuantity());
-//    }
+
+
+
+
+
+    @Test
+    void removeItem_RemovesItemFromInventoryWithQuantityZero() throws URISyntaxException, IOException {
+        Path timtamImagePath = Paths.get(Objects.requireNonNull(getClass().getResource("/static/images/timtam.png")).toURI());
+        byte[] timtamImageBytes = Files.readAllBytes(timtamImagePath);
+        Image timtamImage = new Image(timtamImageBytes, "png", false);
+        BadgeItem timtamBadge = new BadgeItem("Tim Tam", 100, timtamImage, 1);
+
+        User user = new User("John", "Doe", false, "", "", "");
+        user.addItem(timtamBadge);
+        user.removeItem(timtamBadge, 0);
+        assertEquals(1, user.getInventory().size());
+        assertNotNull(user.getItem(timtamBadge, 1));
+        assertEquals(1, user.getItem(timtamBadge, 1).getQuantity());
+    }
+
+    @Test
+    void removeItem_RemovesItemFromInventoryWithQuantityGreaterThanInventory() throws IOException, URISyntaxException {
+        Path vegemiteImagePath = Paths.get(Objects.requireNonNull(getClass().getResource("/static/images/vegemite.png")).toURI());
+        byte[] vegemiteImageBytes = Files.readAllBytes(vegemiteImagePath);
+        Image vegemiteImage = new Image(vegemiteImageBytes, "png", false);
+        BadgeItem vegemiteBadge = new BadgeItem("Tim Tam", 100, vegemiteImage, 1);
+
+        User user = new User("John", "Doe", false, "", "", "");
+        user.addItem(vegemiteBadge);
+        user.addItem(vegemiteBadge);
+        user.addItem(vegemiteBadge);
+        user.addItem(vegemiteBadge);
+        assertThrows(IllegalArgumentException.class, () -> user.removeItem(vegemiteBadge, 5));
+        assertNotNull(user.getItem(vegemiteBadge, 4));
+        assertEquals(4, user.getItem(vegemiteBadge, 4).getQuantity());
+    }
 }
